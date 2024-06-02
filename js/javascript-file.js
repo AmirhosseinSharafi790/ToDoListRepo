6f2e38a37f409bfb7b58c568b3d1afc80755457e
AmirHosseinSharafi <theamir790@gmail.com>
2024-06-02 19:42:12 +0330

    edit trash icon

--- js/javascript-file.js
+++ js/javascript-file.js
@@ -1,53 +1,51 @@
-window.addEventListener('load' , showTasks);
-let addbtn = document.querySelector('button');
-let tasklist = document.querySelector('ul');
-let input = document.querySelector('input');
+window.addEventListener("load", showTasks);
+let addbtn = document.querySelector("button");
+let tasklist = document.querySelector("ul");
+let input = document.querySelector("input");
 let tasks;
-if(!localStorage.getItem('todo')){
-    tasks = [];
+if (!localStorage.getItem("todo")) {
+  tasks = [];
+} else {
+  tasks = getTasks();
 }
-else{
-    tasks = getTasks();
-}
-addbtn.addEventListener('click' , ()=>{
-    let text = input.value;
-    let task = createtask(text);
-    input.value = '';
-    saveTasks(text);
-    if(text == ''){
-        task.remove();
-    }
-})
+addbtn.addEventListener("click", () => {
+  let text = input.value;
+  let task = createtask(text);
+  input.value = "";
+  saveTasks(text);
+  if (text == "") {
+    task.remove();
+  }
+});
 
-tasklist.addEventListener('click' , (e)=>{
-    if(e.target.nodeName === 'I'){
-        let target = e.target.parentElement.parentElement;
-        target.style = 'display : none';
-        tasks.splice(tasks.indexOf(target.textContent) , 1);
-        localStorage.setItem('todo' , tasks);
-    }
-    if(e.target.nodeName === 'LI'){
-        e.target.classList.toggle('done');
-    }
+tasklist.addEventListener("click", (e) => {
+  if (e.target.nodeName === "I") {
+    let target = e.target.parentElement.parentElement;
+    target.style = "display : none";
+    tasks.splice(tasks.indexOf(target.textContent), 1);
+    localStorage.setItem("todo", tasks);
+  }
+  if (e.target.nodeName === "LI") {
+    e.target.classList.toggle("done");
+  }
+});
 
-})
-
-function createtask(text){
-    let li = document.createElement('li');
-    li.textContent += text;
-    li.innerHTML += '<span class="closeBtn"><i class="fa-thin fa-trash"></i></span>';
-    tasklist.appendChild(li);
-    return li;
+function createtask(text) {
+  let li = document.createElement("li");
+  li.textContent += text;
+  li.innerHTML += '<span class="closeBtn"><i class="bi bi-trash3"></i></span>';
+  tasklist.appendChild(li);
+  return li;
 }
-function saveTasks(text){
-    tasks.push(text);
-    localStorage.setItem('todo',tasks);
+function saveTasks(text) {
+  tasks.push(text);
+  localStorage.setItem("todo", tasks);
 }
-function getTasks(){
-    return localStorage.getItem('todo').split(',');
+function getTasks() {
+  return localStorage.getItem("todo").split(",");
 }
-function showTasks(){
-    for (let taskText of getTasks()) {
-        let task = createtask(taskText);
-    }
+function showTasks() {
+  for (let taskText of getTasks()) {
+    let task = createtask(taskText);
+  }
 }
